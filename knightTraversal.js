0c5b5009a5f472bb489c5562733fbaa7dcef8359
Owen Lewis <owenalewis@gmail.com>
2023-09-02 16:39:33 +0100

    Writes knightMoves and placeKnight functions

--- knightTraversal.js
+++ knightTraversal.js
@@ -13,6 +13,58 @@ function createBoard() {
   return board; // Return the 8x8 array
 }
 
+function knightMoves(start, end) {
+  const queue = [];
+  const visited = new Set();
+  queue.push([start, [start]]); // Enqueue the starting position and the path taken so far
+
+  while (queue.length > 0) {
+    const currentPosition = queue.shift()[0]; // The start/move position
+    const path = queue[1]; // Array with the current path
+
+    if ((currentPosition = end)) {
+      return path; // Found the shortest path
+    }
+
+    placeKnight(currentPosition).forEach((move) => {
+      // If move has not been visited
+      if (!visited.has(move)) {
+        visited.add(move);
+        const newPath = path.concat([move]); // Append the new move to the current path
+        queue.push([move, newPath]);
+      }
+    });
+  }
+
+  return "No path found"; // If we exit the loop without returning, no path exists
+}
+
+function placeKnight(position) {
+  const x = currentPosition[0];
+  const y = currentPosition[1];
+  const allPossibleMoves = [
+    (2, 1),
+    (1, 2),
+    (-1, 2),
+    (-2, 1),
+    (-2, -1),
+    (-1, -2),
+    (1, -2),
+    (2, -1),
+  ];
+  const validMoves = [];
+
+  allPossibleMoves.forEach((move) => {
+    const new_x = move[0];
+    const new_y = move[1];
+    if (0 <= new_x < 8 && 0 <= new_y < 8) {
+      validMoves.push(move);
+    }
+  });
+
+  return validMoves;
+}
+
 /*
 PSEUDOCODE:
 FUNCTION createBoard():
